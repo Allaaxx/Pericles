243423cde82da207c5d454e665d30bcd3dcd141f
Allaaxx <allanrao358@gmail.com>
2024-03-20 20:14:52 -0300

    first commit

---
+++ src/main/java/com/etecja/testingspringbootaplication/Aluno.java
@@ -0,0 +1,30 @@
+package com.etecja.testingspringbootaplication;
+import java.util.Date;
+
+public class Aluno {
+
+		private String matricula;
+		
+		private Integer cod_curso;
+		
+		private String nome;
+		
+		private Integer idade;
+		
+		private String endereco;
+
+		private String email;
+		
+		private String cpf;
+		
+		private String rg;
+		
+		private Date dt_entrada;
+		
+		private Date dt_turmasno;
+		
+		private Character trancado;
+		
+		
+		
+}
---
+++ src/main/java/com/etecja/testingspringbootaplication/Curso.java
@@ -0,0 +1,11 @@
+package com.etecja.testingspringbootaplication;
+import java.util.Date;
+
+
+public class Curso {
+	private Integer cod_curso;
+	private String nome;
+	private Character ativo;
+	private Date dt_ativacao;
+	private Date dt_desativacao;
+}
